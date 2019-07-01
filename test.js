5b37b7e5928066c7331116bdbfb75947b7531532
zuqiang.you <zuqiang.you@tongdun.cn>
2019-07-01 10:16:40 +0800

    add test.js

---
+++ test.js
@@ -0,0 +1,41 @@
+// 大致使用dome
+
+import Drag from './drag';
+
+const drag = new Drag;
+
+/* eslint-disable */
+let el = document.querySelector('body');
+// 拖拽目标放置点,可以知己设置
+/* eslint-enable */
+el.addEventListener(
+  'dragover',
+  e => {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+  false
+);
+el.addEventListener(
+  'dragenter',
+  e => {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+  false
+);
+el.addEventListener(
+  'drop',
+  e => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.dataTransfer) {
+      e.dataTransfer.dropEffect = 'copy';
+    }
+    drag.initFiles(e).then(files => {
+      console.log(file);
+      // 获得拖拽过来的文件一维数组信息
+    });
+  },
+  false
+);
